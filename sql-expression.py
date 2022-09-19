efc65b3cbcc521d694559749cbd171b89a829bbf
John <John.Arscott@hotmail.com>
2022-09-19 17:46:06 +0000

    playground

--- sql-expression.py
+++ sql-expression.py
@@ -43,10 +43,12 @@ with db.connect() as connection:
     # select_query = artist_table.select()
 
     # Query 2 - select only the "Name" column from the "Artist" table
-    # select_query = artist_table.select().with_only_columns([artist_table.c.Name])
+    # select_query = artist_table.select().with_only_columns
+    # ([artist_table.c.Name])
 
     # Query 3 - select only 'Queen' from the "Artist" table
-    # select_query = artist_table.select().where(artist_table.c.Name == "Queen")
+    # select_query = artist_table.select().where
+    # (artist_table.c.Name == "Queen")
 
     # Query 4 - select only by 'ArtistId' #51 from the "Artist" table
     # select_query = artist_table.select().where(artist_table.c.ArtistId == 51)
